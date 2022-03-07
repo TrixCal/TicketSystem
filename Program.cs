ef5d67bdde697712cd9479f8fb0f0158222f3a0f
Trayton Larson <traytonlarson01@gmail.com>
2022-03-06 23:51:09 -0600

    Rewrote Program to use new classes

--- Program.cs
+++ Program.cs
@@ -6,20 +6,10 @@ namespace TicketSystem
 {
     class Program
     {
-        static string[] FileToArr(string file){
-            StreamReader sr = new StreamReader(file);
-            List<string> strings = new List<string>();
-            while(!sr.EndOfStream){
-                strings.Add(sr.ReadLine());
-              }
-            sr.Close();
-            return strings.ToArray();
-        }
         static void Main(string[] args)
         {
-            string file = "Tickets.txt";
+            TicketFile ticketFile = new TicketFile("Tickets.txt");
             string choice;
-            Random random = new Random();
             do{
                 //menu prompt
                 Console.WriteLine("1) View Current Tickets");
@@ -29,105 +19,44 @@ namespace TicketSystem
                 //user input
                 choice = Console.ReadLine();
                 if(choice == "1"){
-                    //Read file and writeline tickets
-                    if(File.Exists(file)){
-                        string[] lines = FileToArr(file);
-                        foreach(string line in lines){
-                            string[] arr = line.Split(',');
-                            Console.WriteLine($"{arr[0]}: Summary) {arr[1]}; Status) {arr[2]}; Priority) {arr[3]}; Submitter) {arr[4]}; Assigned) {arr[5]}; Watching) {arr[6]}");
-                            Console.WriteLine("---------------------");
-                        }
-                    }
-                    else{
-                        Console.WriteLine("File doesn't exist, add a ticket to begin.");
-                    }
+                    //Read file and writeline ticket
+                    ticketFile.Display();
                 }
                 if(choice == "2"){
-                    //Prompt ticket elements and write to file
-                    string[] newTicket = new string[7];
-                    //Generates a random id and checks if it doens't exist in the file
-                    int id;
-                    bool isAvailable;
-                    do{
-                        isAvailable = false;
-                        id = random.Next(1, 999);
-                        if(File.Exists(file)){
-                            string[] lines = FileToArr(file);
-                            foreach(string line in lines){
-                                string[] arr = line.Split(',');
-                                if(id.ToString() == arr[0]) isAvailable = true;
-                            }
-                        }
-                    }while(isAvailable);
-                    newTicket[0] = id.ToString();
-                    //Summary
+                    //Prompt ticket elements
+                    Ticket ticket = new Ticket();
+                    ticket.ID = 0;
                     Console.Write("Summary: ");
-                    newTicket[1] = Console.ReadLine();
-                    //Status
+                    ticket.Summary = Console.ReadLine();
                     Console.Write("Status: ");
-                    newTicket[2] = Console.ReadLine();
-                    //Priority
+                    ticket.Status = Console.ReadLine();
                     Console.Write("Priority: ");
-                    newTicket[3] = Console.ReadLine();
-                    //Submitter
+                    ticket.Priority = Console.ReadLine();
                     Console.Write("Submitter: ");
-                    newTicket[4] = Console.ReadLine();
-                    //Assigned
+                    ticket.Submitter = Console.ReadLine();
                     Console.Write("Assigned: ");
-                    newTicket[5] = Console.ReadLine();
-                    //Watching
-                    Console.Write("Watching(Type \"n\" To Stop): ");
-                    string person = Console.ReadLine();
-                    while(person.ToLower() != "n"){
-                        if(newTicket[6] == null)
-                            newTicket[6] += person;
-                        else newTicket[6] += "|" + person;
-                        person = Console.ReadLine();
-                    }
-                    
-                    //Writes ticket to document
-                    string ticketOutput = "";
-                    foreach(string i in newTicket){
-                        if(ticketOutput == "") ticketOutput += i;
-                        else ticketOutput += "," + i;
-                    }
-                    StreamWriter sw = new StreamWriter(file, true);
-                    sw.WriteLine(ticketOutput);
-                    sw.Close();
-
+                    ticket.Assigned = Console.ReadLine();
+                    Console.Write("Watching (type \"n\" to stop): ");
+                    string line = Console.ReadLine();
+                    List<string> names = new List<string>();
+                    do{
+                        names.Add(line);
+                        line = Console.ReadLine();
+                    }while(line != "n");
+                    ticket.Watching = names;
+                    //add ticket to file
+                    ticketFile.AddTicket(ticket);
                 }
                 if(choice == "3"){
                     //Removes ticket by ID
-                    if(File.Exists(file)){
-                        string[] tickets = FileToArr(file);
-                        //Prompt
-                        Console.Write("What ticket would you like to delete?(ID): ");
-                        //Checks if ticket exists
-                        string id;
-                        int i = 0;
-                        bool ticketExists = false;
-                        do{
-                            id = Console.ReadLine();
-                            ticketExists = false;
-                            for(i = 0; i < tickets.Length; i++){
-                                string[] arr = tickets[i].Split(',');
-                                if(id == arr[0]){
-                                    ticketExists = true;
-                                    break;
-                                }
-                            }
-                            if(!ticketExists) Console.WriteLine("Ticket doesn't exist.");
-                        }while(!ticketExists);
-                        //Writes new file
-                        StreamWriter sw = new StreamWriter(file, false);
-                        for(int a = 0; a < tickets.Length; a++){
-                            if(a != i){
-                                sw.WriteLine(tickets[a]);
-                            }
-                        }
-                        sw.Close();
+                    Console.Write("ID of ticket to remove: ");
+                    int ticketID = int.Parse(Console.ReadLine());
+                    if(ticketFile.RemoveTicket(ticketID)){
+                        Console.WriteLine($"Ticket #{ticketID} succesfully removed!");
                     }
-
+                    else{
+                        Console.WriteLine($"Unable to find ticket #{ticketID}");
+                    }
                 }
             }while(choice == "1" || choice == "2" || choice == "3");
         }
