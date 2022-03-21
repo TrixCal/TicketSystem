9522feeed8ba10369954c33944578fb68cba811d
TrixCal <traytonlarson01@gmail.com>
2022-03-21 14:46:47 -0500

    Abstracted Ticket class to add new child classes

--- Ticket.cs
+++ Ticket.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic; 
 
 namespace TicketSystem{
-    class Ticket{
+    abstract class Ticket{
         public int ID { get; set; }
         public string Summary { get; set; }
         public string Status { get; set; }
@@ -14,11 +14,51 @@ namespace TicketSystem{
         public Ticket(){
             Watching = new List<string>();
         }
-        public string Display(){
+        public virtual string Display(){
             return $"Id: {ID}\nSummary: {Summary}\nStatus: {Status}\nPriority: {Priority}\nSubmitter: {Submitter}\nAssigned: {Assigned}\nWatching: {string.Join(", ", Watching)}\n";
         }
         public override string ToString(){
             return $"{ID},{Summary},{Status},{Priority},{Submitter},{Assigned},{string.Join("|", Watching)}";
+        }
+    }
+
+    class Bug : Ticket{
+        public string Severity { get; set; }
+        
+        public override string Display(){
+            return $"Id: {ID}\nSummary: {Summary}\nStatus: {Status}\nPriority: {Priority}\nSubmitter: {Submitter}\nAssigned: {Assigned}\nWatching: {string.Join(", ", Watching)}\nSeverity: {Severity}\n";
+        }
+        public override string ToString()
+        {
+            return $"{ID},{Summary},{Status},{Priority},{Submitter},{Assigned},{string.Join("|", Watching)},{Severity}";
+        }
+    }
+
+    class Enhancement : Ticket{
+        public string Software { get; set; }
+        public double Cost { get; set; }
+        public string Reason { get; set; }
+        public double Estimate { get; set; }
+
+        public override string Display(){
+            return $"Id: {ID}\nSummary: {Summary}\nStatus: {Status}\nPriority: {Priority}\nSubmitter: {Submitter}\nAssigned: {Assigned}\nWatching: {string.Join(", ", Watching)}\nSoftware: {Software}\nCost: {Cost:C}\nReason: {Reason}\nEstimate: {Estimate:C}\n";
+        }
+        public override string ToString()
+        {
+            return $"{ID},{Summary},{Status},{Priority},{Submitter},{Assigned},{string.Join("|", Watching)},{Software},{Cost},{Reason},{Estimate}";
+        }
+    }
+
+    class Task : Ticket{
+        public string ProjectName { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public override string Display(){
+            return $"Id: {ID}\nSummary: {Summary}\nStatus: {Status}\nPriority: {Priority}\nSubmitter: {Submitter}\nAssigned: {Assigned}\nWatching: {string.Join(", ", Watching)}\nProject Name: {ProjectName}\nDue By: {DueDate:d}\n";
+        }
+        public override string ToString()
+        {
+            return $"{ID},{Summary},{Status},{Priority},{Submitter},{Assigned},{string.Join("|", Watching)},{ProjectName},{DueDate}";
         }
     }
 }
